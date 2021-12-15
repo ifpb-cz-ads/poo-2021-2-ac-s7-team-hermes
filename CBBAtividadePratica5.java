f01243f018d8e604af2c595443e2faef64deeffa
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2021-12-15 19:15:31 -0300

    Update CBBAtividadePratica5.java

--- CBBAtividadePratica5.java
+++ CBBAtividadePratica5.java
@@ -1,4 +1,4 @@
-public class NomesDiasdaSemana
+public class CBBAtividadePratica5
 {
     public static void main(String[] args){
         String[] dias = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"};
