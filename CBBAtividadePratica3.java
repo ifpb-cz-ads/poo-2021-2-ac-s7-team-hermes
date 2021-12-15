f69c9e917fab30e1ae6248c55ce9988d827726d0
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2021-12-15 18:50:23 -0300

    Create CBBAtividadePratica3.java

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,13 @@
+public class CBBAtividadePratica3
+{
+    public static void main(String[] args){
+        int[] num = new int[100];
+        int i;
+        for (i=0; i<num.length; i++){
+            num[i] = i;
+        }
+        for (i=0; i<num.length; i++){
+            System.out.println(num[i]);
+        }
+    }
+}
